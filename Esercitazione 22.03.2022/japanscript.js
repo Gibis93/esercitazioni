a7d378b39c9987aa5bf2a17974b432c50869440a
Giulio Bozzotta <101398781+Gibis93@users.noreply.github.com>
2022-03-22 22:06:29 +0100

    Add files via upload

---
+++ Esercitazione 22.03.2022/japanscript.js
@@ -0,0 +1,10 @@
+let firstNum;
+let secondNum;
+
+firstNum = parseInt (prompt ("numero 1"));
+secondNum = parseInt (prompt ("numero 2"));
+
+const operazione = () => alert (firstNum + secondNum);
+
+setTimeout(operazione, 10000);
+    
